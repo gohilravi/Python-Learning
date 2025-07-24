767bed41181dd3136ea63c8652c49351047064e0
Ravi <ravi.gohil@marutitech.com>
2025-07-24 12:39:02 +0530

    Assignment 1 task

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,9 @@
+﻿firstNumber= float(input('Enter the first number: '))
+secondNumber = float(input('Enter the second number: '))
+
+print('Addition:', firstNumber + secondNumber)
+print('Subtraction:', firstNumber - secondNumber)
+print('Multiplication:', firstNumber * secondNumber)
+print('Division:', firstNumber / secondNumber)
+
+print('End of task. Thank you!')
